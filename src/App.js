dee347fd2107b37c6fe47f71850655e1bb1d0908
MoeMixMC <moabdalla246@gmail.com>
2022-04-21 14:52:50 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { init, getOwnBalance } from './Web3Client'
+
+function App() {
+
+  const [balance, setBalance] = useState(0)
+
+  const fetchBalance = () => {
+    getOwnBalance().then(balance => {
+      setBalance(balance)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  return (
+    <div className="App">
+      <p> Balance is {balance} </p>
+      <button onClick={() => fetchBalance()}>Refresh Balance</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Web3Client.js
@@ -0,0 +1,102 @@
+// import NFTContractBuild from 'contracts/NFT.json';
+import Web3 from 'web3'
+import axios from 'axios'
+import 'dotenv/config'
+
+//const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS
+const NFT_CONTRACT_ADDRESS = '0xf8D4fEf9af82De6E57F6aABAFd49ff9730242d75'
+
+let selectedAccount;
+
+// let nftContract;
+let nftContract;
+
+let isInitialized = false;
+
+export const init = async () => {
+	let provider = window.ethereum;
+
+	if (typeof provider !== 'undefined') {
+		provider
+			.request({ method: 'eth_requestAccounts' })
+			.then((accounts) => {
+				selectedAccount = accounts[0];
+				console.log(`Selected account is ${selectedAccount}`);
+			})
+			.catch((err) => {
+				console.log(err);
+				return;
+			});
+
+		window.ethereum.on('accountsChanged', function (accounts) {
+			selectedAccount = accounts[0];
+			console.log(`Selected account changed to ${selectedAccount}`);
+		});
+	}
+
+	const web3 = new Web3(provider);
+
+	const networkId = await web3.eth.net.getId();
+
+	// nftContract = new web3.eth.Contract(
+	// 	NFTContractBuild.abi,
+	// 	NFTContractBuild.networks[networkId].address
+	// );
+
+	 /* const abi = [
+		{
+			constant: true,
+			inputs: [
+				{
+					name: '_owner',
+					type: 'address'
+				}
+			],
+			name: 'balanceOf',
+			outputs: [
+				{
+					name: 'balance',
+					type: 'uint256'
+				}
+			],
+			payable: false,
+			stateMutability: 'view',
+			type: 'function'
+		}
+	];  */
+
+    let response = await axios.get('https://api.etherscan.io/api?module=contract&action=getabi&address=' + NFT_CONTRACT_ADDRESS + '&apikey=9SQVDEWPVU54IQW67IFQCNVG87H5EBTXJT');
+    let data = response.data
+
+    // create the smart contract JSON ABI
+    let abi = data.result;
+    console.log('ABI created')
+
+	nftContract = new web3.eth.Contract(
+		JSON.parse(abi),
+		// Dai contract on Rinkeby
+		NFT_CONTRACT_ADDRESS
+	);
+
+	isInitialized = true;
+};
+
+export const getOwnBalance = async () => {
+	if (!isInitialized) {
+		await init();
+	}
+
+	return nftContract.methods
+		.maxSupply()
+		.call()
+};
+
+// export const mintToken = async () => {
+// 	if (!isInitialized) {
+// 		await init();
+// 	}
+
+// 	return nftContract.methods
+// 		.mint(selectedAccount)
+// 		.send({ from: selectedAccount });
+// };
